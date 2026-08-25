2f0230df6b9c4ba8d248f45ed79401fd39e9a5f3
HMassinissa92 <hmassinissa39@gmail.com>
2026-08-25 21:33:46 +0100

    proxy api via vercel pour resoudre cookies cross-domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -10,7 +9,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://pladichat-backend.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
-
 
 export default nextConfig;
